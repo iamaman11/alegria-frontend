9cf919bcafecafd9370d24accb31005cd4c975a4
iamaman11 <majakojh@gmail.com>
2025-10-17 03:17:18 +0300

    fix: update robots.txt to block all pages

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
     policies: [
       {
         userAgent: '*',
-        disallow: '/admin/*',
+        disallow: '/',
       },
     ],
     additionalSitemaps: [`${SITE_URL}/pages-sitemap.xml`, `${SITE_URL}/posts-sitemap.xml`],
